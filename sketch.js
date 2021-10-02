b1ed6cc7ac4040c0062b82186a24b399c2fe084f
vaibhavkrishnanwh <88144325+vaibhavkrishnanwh@users.noreply.github.com>
2021-10-02 18:05:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,122 @@
+  
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+var end;
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup() {
+  createCanvas(600,600);
+  spookySound.loop();
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+  doorsGroup = new Group();
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
+  
+  ghost = createSprite(200,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
+}
+
+
+function draw() {
+  background(255);
+  
+  if (gameState === "play") {
+    
+    if(keyDown("LEFT_ARROW")){
+    ghost.x = ghost.x-3;
+      // write a code to move left when left arrow is pressed
+    }
+    if(keyDown("RIGHT_ARROW")){
+      ghost.x = ghost.x+3;
+      // write a code to move left when right arrow is pressed
+      
+    }
+    if(keyDown("SPACE")){
+      ghost.velocityY = ghost.velocityY + 0.8;
+   
+      // write a code to move up when space arrow is pressed
+      
+    }
+  
+  ghost.velocityY = ghost.velocityY + 0.8;
+  
+   
+      //write a condition for infinte scrolling tower
+      if(tower.y>400){
+       tower.y = 300;
+      }
+    
+      spawnDoors();
+
+  
+      //write a code to make climbersGroup collide with ghost change the ghost velocity
+      if(climbersGroup.isTouching(ghost)){
+        ghost.velocity = 0;
+
+      }
+      //write a code to make invisibleBlockGroup collide with ghost destroy the ghost and make gamestate to end.
+      if(invisibleBlockGroup.collide(ghost)){
+      ghost.destroy();
+      gameState == end; 
+      }
+  drawSprites();
+}
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Game Over", 230,250)
+  }
+}
+
+function spawnDoors()
+ {
+  //write code here to spawn the clouds
+  if (frameCount % 240 === 0) {
+    var door = createSprite(200, -50);
+    var climber = createSprite(200,10);
+    var invisibleBlock = createSprite(200,15);
+    invisibleBlock.width = climber.width;
+    invisibleBlock.height = 2;
+    //add the random function
+    //
+    door.addImage(doorImg);
+    climber.addImage(climberImg);
+    
+    door.velocityY = 1;
+    climber.velocityY = 1;
+    invisibleBlock.velocityY = 1;
+
+    //change the depth of the ghost and door
+    
+     
+
+    
+    //assign lifetime to the obstacle.lifetime = 300; here  obstacle are door, climber and invisible block
+    doorsGroup.lifetime = 300;
+    climbersGroup.lifetime = 300;
+    invisibleBlockGroup.lifetime = 300;
+
+    //add each obstacle to the group obstaclesGroup.add(obstacle);here  obstacle are door, climber and invisible block
+    doorsGroup.add(door);
+    climbersGroup.add(climber);
+    invisibleBlockGroup.add(invisibleBlock);
+  
+  }
+}
+
